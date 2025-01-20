af97de4cc68c02525fb29e88c90448e6b5d19419
Justin <justinperrone1998@gmail.com>
2025-01-20 19:41:29 +0100

    created landing page

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,18 +1,16 @@
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 
 export default function HeaderBar() {
+  
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
+          {/* <IconButton
             size="large"
             edge="start"
             color="secondary"
@@ -20,11 +18,11 @@ export default function HeaderBar() {
             sx={{ mr: 2 }}
           >
             <MenuIcon />
-          </IconButton>
+          </IconButton> */}
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Justins Projects
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit">See whats next</Button>
         </Toolbar>
       </AppBar>
     </Box>
